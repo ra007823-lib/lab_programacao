558f2cf06a87614d8031936d58dbc32a6d86f503
ra007823-lib <007823@libertas.edu.br>
2026-06-03 22:35:41 -0300

    update

--- labprog/2 bimestre/Trabalho.04.06.26/ex008.py
+++ labprog/2 bimestre/Trabalho.04.06.26/ex008.py
@@ -1,6 +1,7 @@
 x = int(input("Digite o dividendo (x): "))
 y = int(input("Digite o divisor (y): "))
-
+div = x
+divisor = y
 quociente = 0
 
 while x >= y:
@@ -9,4 +10,4 @@ while x >= y:
 
 resto = x
 
-print(f"O resultado do {x} / {y} = {quociente}, com resto {resto}.")
+print(f"O resultado do {div} / {divisor} = {quociente}, com resto {resto}.")
