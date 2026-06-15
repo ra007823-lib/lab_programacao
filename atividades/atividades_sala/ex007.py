fadb6d0074252f4331293a988e454f9b31a46645
ra007823-lib <007823@libertas.edu.br>
2026-06-15 19:39:49 -0300

    atualização pasta atividades 15 06 26

--- atividades/atividades_sala/ex007.py
+++ atividades/atividades_sala/ex007.py
@@ -1,1 +1,19 @@
 #eleabore um programa que leia 10 numeros inteiros do teclado. á medida que os numeros forem lidos, os pares devem ser inseridos em uma lista chamada pares e os impares em uma lista chamada impares. porem se o usuario digitar um numero que já foi inserido anteriormente em qualquer uma das listas, o programa deve recusar o numero e pedir para digitar outro.
+
+pares = []
+impares = []
+
+while len(pares) + len(impares) < 10:
+    numero = int(input("Digite um número inteiro: "))
+
+    if numero in pares or numero in impares:
+        print("Número já informado! Digite outro.")
+        continue
+    
+    if numero % 2 == 0:
+        pares.append(numero)
+    else:
+        impares.append(numero)
+
+print("\nLista de pares:", pares)
+print("Lista de ímpares:", impares)
--- atividades/atividades_sala/ex008.py
+++ atividades/atividades_sala/ex008.py
@@ -1,1 +1,18 @@
 #sem usar comando .split() nativo Python, faça um programa que receba uma frase curta separada por espaços(ex: 'Banco de dados')  e transforme cada palavra em um elemento de um vetor (ex:['Banco','de','dados']).
+frase = input("Digite uma frase: ")
+
+vetor = []
+palavra = ""
+
+for caractere in frase:
+    if caractere != " ":
+        palavra += caractere
+    else:
+        if palavra != "":
+            vetor.append(palavra)
+            palavra = ""
+
+if palavra != "":
+    vetor.append(palavra)
+
+print(vetor)
