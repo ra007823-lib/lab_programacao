b004e52a82b0e472a99e5685e805996ce60960cb
ra007823-lib <007823@libertas.edu.br>
2026-06-18 21:12:20 -0300

    atualização ex009

--- atividades/atividades_sala/ex009.py
+++ atividades/atividades_sala/ex009.py
@@ -7,11 +7,15 @@ while True:
         break
     hist.append(valor)
 
-hist= [x for x in hist if x>= 5]
+#hist= [x for x in hist if x>= 5]
+
+for i in range(len(hist) -1, -1, -1):
+    if abs(hist[i]) > -5 and abs(hist[i]) < 5:
+        del hist[i]
 
 print('-'*25)
 print('O histórico do seu saldo é:')
 for valor in hist:
-    print('-'*25)
-    print(f'\t\033[32mR${valor:.2f}\033[m')
+        print('-'*25)
+        print(f'\t\033[33mR${valor:.2f}\033[m')
 print('\n')
